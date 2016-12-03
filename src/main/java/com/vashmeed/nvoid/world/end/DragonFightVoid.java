37f1f9f6c55ab3ddfd5ec3e924613fdb247fa9b0
Vashmeed <vnosrati@live.com>
2016-12-03 01:07:04 -0800

    Dragon fight
    - Added base for custom dragon fight for void

---
+++ src/main/java/com/vashmeed/nvoid/world/end/DragonFightVoid.java
@@ -0,0 +1,13 @@
+package com.vashmeed.nvoid.world.end;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.WorldServer;
+import net.minecraft.world.end.DragonFightManager;
+
+public class DragonFightVoid extends DragonFightManager {
+
+	public DragonFightVoid(WorldServer worldIn, NBTTagCompound compound) {
+		super(worldIn, compound);
+	}
+
+}
